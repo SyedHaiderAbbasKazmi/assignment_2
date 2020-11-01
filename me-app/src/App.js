6d697e67deb33c6318392a9a991533a2dc07266e
u2017456@giki.edu.pk <u2017456@giki.edu.pk>
2020-11-01 17:32:32 +0500

    text changes

--- me-app/src/App.js
+++ me-app/src/App.js
@@ -7,8 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Assignment 2 app 
-          Edit <code>src/App.js</code> and save to reload.
+          Here is the assigment 2 text changes
         </p>
         <a
           className="App-link"
